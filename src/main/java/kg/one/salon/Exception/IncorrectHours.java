d1a503b1e578aa6148ca240111f05e5f7663ff91
Amalbekov96 <amalbekov312@gmail.com>
2021-08-07 00:43:54 -0500

    Working on the project

---
+++ src/main/java/kg/one/salon/Exception/IncorrectHours.java
@@ -0,0 +1,7 @@
+package kg.one.salon.Exception;
+
+public class IncorrectHours extends RuntimeException{
+    public IncorrectHours(String message) {
+        super(message);
+    }
+}
--- src/main/java/kg/one/salon/Service/ReservedHoursService.java
+++ src/main/java/kg/one/salon/Service/ReservedHoursService.java
@@ -1,6 +1,7 @@
 package kg.one.salon.Service;
 
 import kg.one.salon.Exception.EntityNotFound;
+import kg.one.salon.Exception.IncorrectHours;
 import kg.one.salon.Model.Dto.ReservedHoursDto;
 import kg.one.salon.Model.Mapper.ReservedHoursMapper;
 import kg.one.salon.Model.ReservedHours;
@@ -35,11 +36,45 @@ public class ReservedHoursService extends BaseServiceImpl<ReservedHours, Reserve
     public ResponseEntity<?> create(ReservedHoursDto reservedHoursDto) {
         List<ReservedHoursDto> dtoList = mapper.toListDto(repo.findAllByWorkDayIdId(reservedHoursDto.getWorkDayId().getId()));
 
+        if(reservedHoursDto.getEndTime().getMinute() > 5 || reservedHoursDto.getStartTime().getMinute() > 5){
+            throw new IncorrectHours("Please try to put only rounded times like 11 : 00 - 12 : 00 ");
+        }
+
+        if(reservedHoursDto.getStartTime().getHour() == reservedHoursDto.getEndTime().getHour()){
+            throw  new IncorrectHours("Please make sure you have chosen correct range!");
+        }
 
         for(ReservedHoursDto d : dtoList){
 
-//            if(d.getWorkDayId().getStartTime().toLocalTime()
+            if(d.getWorkDayId().getStartTime().getHour() > reservedHoursDto.getStartTime().getHour()){
+                throw new IncorrectHours("Master does not work at this hour!");
+            }
+
+            if(d.getWorkDayId().getEndTime().getHour() < reservedHoursDto.getEndTime().getHour()){
+                throw  new IncorrectHours("Master does not work at this hour!");
+            }
+
+            if(d.getStartTime().getHour() == reservedHoursDto.getStartTime().getHour() && d.getEndTime().getHour() == reservedHoursDto.getEndTime().getHour()){
+                throw  new IncorrectHours("These hours already booked!");
+            }
+
+            if(d.getStartTime().getHour() < reservedHoursDto.getStartTime().getHour() && d.getEndTime().getHour() > reservedHoursDto.getStartTime().getHour()){
+                throw new IncorrectHours("This hour is not correct!");
+            }
+
+            if(d.getStartTime().getHour() < reservedHoursDto.getEndTime().getHour() && d.getEndTime().getHour() > reservedHoursDto.getEndTime().getHour()){
+                throw new IncorrectHours("This hour is not correct!");
+            }
+
+            if(d.getStartTime().getHour() == reservedHoursDto.getStartTime().getHour() && d.getEndTime().getHour() < reservedHoursDto.getEndTime().getHour()){
+                throw new IncorrectHours("These hours already booked!");
+            }
+
+            if(d.getEndTime().getHour() == reservedHoursDto.getEndTime().getHour() && d.getStartTime().getHour() > reservedHoursDto.getStartTime().getHour()){
+                throw new IncorrectHours("These hours already booked!");
+            }
         }
+
         return super.create(reservedHoursDto);
     }
 }
